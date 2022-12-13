b02fc067ec2d39b022fded47e955a6d5e545f747
shreyni <120514315+shreyni@users.noreply.github.com>
2022-12-13 14:49:46 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 6
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
+from pmdarima.arima import auto_arima
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import math
+import numpy as np
+import quandl
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+from pandas.plotting import lag_plot
+import statsmodels.api as sm
+import yfinance as yf
+import requests
